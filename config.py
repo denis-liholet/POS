72d8cea367102141ea4bc0634dfc4ec5758088e0
Denis Liholet <palloncino.vin@gmail.com>
2021-06-02 19:23:17 +0300

    add email notification in error.py

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
-class Configuration:
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///resources/pos.db'
-    SQLALCHEMY_MIGRATE_REPO = '/migrations'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'some secret salt)'
+# flask app settings
+
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///resources/pos.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'some secret salt)'
+
+# mail settings
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'flask.logging@gmail.com'
+MAIL_DEFAULT_SENDER = 'flask.logging@gmail.com'
+MAIL_PASSWORD = 'Qaz123456!'
+RECEPIENTS = ['palloncino.vin@gmail.com']
--- views/error.py
+++ views/error.py
@@ -1,10 +1,30 @@
+import sys
+import traceback
+
+from flask_mail import Mail, Message
+
 from app import app
+from config import RECEPIENTS
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
+
+    # error information collecting and preparing message body
+    tb_object = sys.exc_info()[2]
+    tb_info = traceback.format_tb(tb_object)
+    tb_info = ''.join([str(item) for item in tb_info])
+    info = str(tb_info) + str(sys.exc_info()[1])
+
+    # sending an email with error description
+    mail = Mail(app)
+    msg = Message('Internal server error: 500', recipients=RECEPIENTS)
+    msg.body = info
+    mail.send(msg)
+
     message_1 = '<h1>An unexpected error has occurred</h1>'
     message_2 = '<p>The administrator has been notified. Sorry for the inconvenience!</p>'
+
     return message_1 + message_2, 500
 
 
